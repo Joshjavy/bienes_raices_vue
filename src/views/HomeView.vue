313ff761a458f7df491f5a58588717590f1b3b4d
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-06-01 09:28:42 -0600

    262. Creando la Vista de Login

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,5 @@
 </script>
 
 <template>
-  <main>
     <h1 class="text-center">Vue Bienes Raices</h1>
-  </main>
 </template>
---
+++ src/views/Login.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1 class="text-center">Login</h1>
+</template>
