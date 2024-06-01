9fb11fa807c8e6126362335c9c0b48ed88fcf8ec
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-06-01 10:22:25 -0600

    263. Creando el Formulario de Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,5 +3,44 @@
 </script>
 
 <template>
-    <h1 class="text-center">Login</h1>
+    <v-card flat max-width="600" class="mx-auto">
+        <v-card-title
+            class="text-h4 font-weight-bold"
+            tag="h3"
+        >
+            Iniciar Sesión
+
+        </v-card-title>
+
+        <v-card-subtitle>
+            Inicia Sesión con tu cuenta
+        </v-card-subtitle>
+
+        <v-form class="mt-5">
+            <v-text-field
+                type="email"
+                label="Email"
+                bg-color="blue-lighten-4"
+            >
+            </v-text-field>
+
+            <v-text-field
+                type="password"
+                label="Password"
+                bg-color="blue-lighten-4"
+            ></v-text-field>
+
+            <v-btn
+                block
+                color="pink-accent-3"
+            >
+                Ininiciar Sesión
+            </v-btn>
+
+
+        </v-form>
+
+
+    </v-card>
+
 </template>
