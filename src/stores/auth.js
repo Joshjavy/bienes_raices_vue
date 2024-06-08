882101295688a380b80b36d889e1e65d9cb2567f
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-06-08 13:02:23 -0600

    269. Creando un Diccionario de Errores

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -5,13 +5,19 @@ import { signInWithEmailAndPassword } from 'firebase/auth'
 
 export const useAuthStore = defineStore('auth',()=>{
     const auth = useFirebaseAuth()
+    const errorCode = {
+        'auth/invalid-credential':'Credencial invalido'
+    }
+
+
+
     const login =({email,password})=>{
         signInWithEmailAndPassword(auth,email,password)
         .then((response)=>{
             console.log(response)
         })
         .catch(error=>{
-            console.log(error.message)
+            console.log(errorCode[error.code])
         })
     }
 
